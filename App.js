c175be016e2a87e84451939a78de49c9bb153ac2
leonardolima <leonardo.bruno@outlook.com>
2018-08-06 22:41:03 -0300

    create new components card, card section and button

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
-import Header from './src/components/header';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header headerText={'Albums!'} />
+  <View style={{ flex: 1 }}>
+    <Header headerText={'My Favorit Albums'} />
+    <AlbumList />
+  </View>
 );
 
 export default App;
